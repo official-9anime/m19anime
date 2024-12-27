0ad85b49aac4bc9a745e28ba92cda55be3341cce
9anime <changezik42@gmail.com>
2024-12-27 13:40:03 +0500

    Update asurascans.py

--- src/asurascans.py
+++ src/asurascans.py
@@ -12,7 +12,11 @@ class Asurascans:
 
     def fetch_html(self, url):
         try:
-            response = requests.get(f"{self.proxy_url}{url}")
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9",
+            }
+            response = requests.get(f"{self.proxy_url}{url}", headers=headers)
             response.raise_for_status()
             return response.text
         except requests.RequestException as e:
